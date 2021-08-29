e9c013854526fc9df47f60a7a8405376539666e2
Dimkabit <65062995+Dimkabit@users.noreply.github.com>
2021-08-29 16:14:12 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ headerBerger.addEventListener('click', (e) => {
 });
 
 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("up").style.display = "block";
+    } else {
+        document.getElementById("up").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  
+	const upBtn = document.getElementById('up');
+	upBtn.scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	});
+}
